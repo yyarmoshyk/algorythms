4b3076a2a30d780816d1c37d11d1668b22e175a6
Yaroslav Yarmoshyk <y.yarmoshyk@gmail.com>
2022-08-11 12:24:30 +0200

    Added solution for any set of brackets

--- backtracking-perentesis/unit_tests.py
+++ backtracking-perentesis/unit_tests.py
@@ -9,31 +9,40 @@ import validate
 class parentheses(unittest.TestCase):
     @pytest.mark.task(taskno=1)
     def test_validate1(self):
-        self.assertEqual(validate.is_valid1('(())'), True)
-        self.assertEqual(validate.is_valid2('(())'), True)
+        self.assertEqual(validate.is_valid_regular1('(())'), True)
+        self.assertEqual(validate.is_valid_regular2('(())'), True)
 
     @pytest.mark.task(taskno=2)
     def test_validate2(self):
-        self.assertEqual(validate.is_valid1('(()))'), False)
-        self.assertEqual(validate.is_valid2('(()))'), False)
+        self.assertEqual(validate.is_valid_regular1('(()))'), False)
+        self.assertEqual(validate.is_valid_regular2('(()))'), False)
     
     @pytest.mark.task(taskno=3)
     def test_validate3(self):
-        self.assertEqual(validate.is_valid1('(())('), False)
-        self.assertEqual(validate.is_valid2('(())('), False)
+        self.assertEqual(validate.is_valid_regular1('(())('), False)
+        self.assertEqual(validate.is_valid_regular2('(())('), False)
 
     @pytest.mark.task(taskno=4)
     def test_validate4(self):
-        self.assertEqual(validate.is_valid1('()(())'), True)
-        self.assertEqual(validate.is_valid2('()(())'), True)
+        self.assertEqual(validate.is_valid_regular1('()(())'), True)
+        self.assertEqual(validate.is_valid_regular2('()(())'), True)
 
     @pytest.mark.task(taskno=5)
     def test_validate5(self):
-        self.assertEqual(validate.is_valid1('()(()()'), False)
-        self.assertEqual(validate.is_valid2('()(()()'), False)
+        self.assertEqual(validate.is_valid_regular1('()(()()'), False)
+        self.assertEqual(validate.is_valid_regular2('()(()()'), False)
 
     @pytest.mark.task(taskno=6)
     def test_generate(self):
         self.assertEqual(generate.gen(2), ['(())', '()()'])
         self.assertEqual(generate.gen(3), ['((()))', '(()())', '(())()', '()(())', '()()()'])
 
+    @pytest.mark.task(taskno=7)
+    def test_validate7(self):
+        self.assertEqual(validate.is_valid_any('({[]})'), True)
+        self.assertEqual(validate.is_valid_any('({[])})'), False)
+
+    @pytest.mark.task(taskno=8)
+    def test_validate8(self):
+        self.assertEqual(validate.is_valid_any(')({[]})'), False)
+        self.assertEqual(validate.is_valid_any('({[])}){'), False)
--- backtracking-perentesis/validate.py
+++ backtracking-perentesis/validate.py
@@ -1,4 +1,7 @@
-def is_valid1(combination):
+from ast import Return
+
+
+def is_valid_regular1(combination):
     # Initiate empty stack
     stack = []
     # Iterate over chars in combination
@@ -6,7 +9,6 @@ def is_valid1(combination):
         # Append stack if we found and opening (
         if par == '(':
             stack.append(par)
-            print(stack)
         else:
             # Validity condition1: not poping from empty stack
             # Otherwise it meens that we found a closing parentheses without an opening one from it
@@ -19,7 +21,7 @@ def is_valid1(combination):
     # Otherwise it means that we have opening parentheses that we didn't close
     return len(stack) == 0
 
-def is_valid2(combination):
+def is_valid_regular2(combination):
     # Use int as counter
     diff = 0
     # Iterate over chars in combination
@@ -38,3 +40,34 @@ def is_valid2(combination):
     # Validity cond #2: stack should be empty at the end
     # Otherwise it means that we have opening parentheses that we didn't close
     return diff == 0
+
+def is_valid_any(combination):
+    # Initiate the list of openings 
+    openings = ['{','(','[']
+    # Initiate the set of pares closing:opening
+    pares = { 
+            '}':'{', 
+            ')':'(',
+            ']':'['
+        }
+    # initiate the empty list to track openings
+    stack = []
+    
+    # Iterate over chars in combination
+    for par in combination:
+        # add opening to the stack list
+        if par in openings:
+            stack.append(par)
+        # If current char is not an opening than this should be a closing bracket
+        else:
+            # the bracket should be in the pares
+            if par in pares:
+            # Validity condition1: if counter is 0 than we have empty stack and can't decrease it
+            # Otherwise it meens that we found a closing parentheses without an opening one from it
+                if len(stack) == 0:
+                    return False
+                # Pop removes the last record from the list and returns its value.
+                # if the last value we remove from list doesn't correspond the opening of the par iterator in in pares set than open/close doesn't match
+                if stack.pop() != pares[par]:
+                    return False
+    return True
